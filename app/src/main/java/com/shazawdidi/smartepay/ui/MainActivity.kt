9d71b242c8497dd3b3a2487514860edb1669b22b
Shazalia <Muzamil@altkamul.ae>
2021-08-12 14:52:41 +0200

    Initial Setup

---
+++ app/src/main/java/com/shazawdidi/smartepay/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.shazawdidi.smartepay.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.shazawdidi.smartepay.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/shazawdidi/smartepay/ui/tasks/MainTask.kt
@@ -0,0 +1,6 @@
+package com.shazawdidi.smartepay.ui.tasks
+
+import androidx.lifecycle.ViewModel
+
+class MainTask: ViewModel() {
+}
